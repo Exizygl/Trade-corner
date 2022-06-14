b2ce6165913a42d4910c06e69abd318a43067e15
Jordan <jordanmahaut09@gmail.com>
2022-06-15 01:03:17 +0200

    Création du style register

--- src/app/components/layouts/RegisterComponents.jsx
+++ src/app/components/layouts/RegisterComponents.jsx
@@ -4,6 +4,8 @@ import { useFormik } from 'formik';
 const RegisterComponents = () => {
     const initialValues = {
         pseudo: '',
+        firstName: '',
+        surName: '',
         email: '',
         phoneNumber: '',
         adress: '',
@@ -17,64 +19,60 @@ const RegisterComponents = () => {
         initialValues,
     });
 
-    const { pseudo, email, phoneNumber, adress, zipcode, ville, password, cgu } =
-        formik.values;
+    const {
+        pseudo,
+        firstName,
+        surName,
+        email,
+        phoneNumber,
+        adress,
+        zipcode,
+        ville,
+        password,
+        cgu,
+    } = formik.values;
 
     return (
         <div>
-            <h1>Inscription</h1>
+            <h2>Inscription</h2>
             <form className="Register">
-                <div>
+                <div className="label">
                     <label htmlFor="pseudo">Nom d'utilisateur : </label>
-                    <input type="text" name="pseudo" id="pseudo" value={pseudo} />
-                </div>
-
-                <div>
+                    <label htmlFor="firstName">Prénom : </label>
+                    <label htmlFor="surName">Nom : </label>
                     <label htmlFor="email">Email : </label>
-                    <input type="email" name="email" id="email" value={email} />
+                    <label htmlFor="phoneNumber">Numéro de téléphone : </label>
+                    <label htmlFor="adress">Adresse postale : </label>
+                    <label htmlFor="zipcode">Code postale : </label>
+                    <label htmlFor="ville">Ville : </label>
+                    <label htmlFor="password">Mot de passe : </label>
                 </div>
 
-                <div>
-                    <label htmlFor="phoneNumber">Numéro de téléphone</label>
+                <div className="register2">
+                    <input type="text" name="pseudo" id="pseudo" value={pseudo} />
+                    <input
+                        type="text"
+                        name="firstName"
+                        id="firstName"
+                        value={firstName}
+                    />
+                    <input type="text" name="surName" id="surName" value={surName} />
+                    <input type="email" name="email" id="email" value={email} />
                     <input
                         type="tel"
                         name="phoneNumber"
                         id="phoneNumber"
                         value={phoneNumber}
                     />
-                </div>
-
-                <div>
-                    <label htmlFor="adress">Adresse postale : </label>
                     <input type="text" name="adress" id="adress" value={adress} />
-                </div>
-
-                <div>
-                    <div>
-                        <label htmlFor="zipcode">Code postale : </label>
-                        <input type="number" name="number" id="number" value={zipcode} />
-                    </div>
-                    <div>
-                        <label htmlFor="ville">Ville : </label>
-                        <input type="text" name="ville" id="ville" value={ville} />
-                    </div>
-                </div>
-
-                <div>
-                    <div>
-                        <label htmlFor="password">Mot de passe : </label>
-                        <input
-                            type="password"
-                            name="password"
-                            id="password"
-                            value={password}
-                        />
-                    </div>
-
-                    <div>
-                        <input type="checkbox" name="cgu" id="cgu" value={cgu} />
-                        <label htmlFor="cgu"></label>
-                    </div>
+                    <input type="number" name="number" id="number" value={zipcode} />
+                    <input type="text" name="ville" id="ville" value={ville} />
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        value={password}
+                    />
                 </div>
             </form>
         </div>
