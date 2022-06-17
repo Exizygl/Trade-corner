3c2e81d85db31f63dc9f77a213e579dc5974ed75
JordanMahaut <jordanmahaut09@gmail.com>
2022-06-17 10:46:17 +0200

    Register

--- src/app/components/layouts/Register.jsx
+++ src/app/components/layouts/Register.jsx
@@ -35,6 +35,7 @@ const Register = () => {
         <div>
             <div className="global">
                 <form>
+                    <legend className="titre">Inscription</legend>
                     <div>
                         <label htmlFor="pseudo">Pseudonyme : </label>
                         <input
@@ -43,28 +44,110 @@ const Register = () => {
                             id="pseudo"
                             value={pseudo}
                             onChange={formik.handleChange}
+                            required
                         />
-                        <label htmlFor="name">Nom complet: </label>
+                    </div>
+
+                    <div className="Information">
+                        <label htmlFor="name">Nom complet : </label>
                         <input
                             type="text"
                             name="name"
                             id="surName"
                             value={name}
                             onChange={formik.handleChange}
+                            required
+                        />
+
+                        <label htmlFor="phoneNumber">Numéro de téléphone : </label>
+                        <input
+                            type="tel"
+                            name="phoneNumber"
+                            id="phoneNumber"
+                            value={phoneNumber}
+                            onChange={formik.handleChange}
+                            required
                         />
                     </div>
 
-                    <div></div>
+                    <div>
+                        <label htmlFor="adress">Adresse postale : </label>
+                        <input
+                            type="text"
+                            name="adress"
+                            id="adress"
+                            value={adress}
+                            onChange={formik.handleChange}
+                            required
+                        />
+                    </div>
 
                     <div>
-                        <label htmlFor="email">Email : </label>
+                        <label htmlFor="ville">Ville : </label>
+                        <input
+                            type="text"
+                            name="ville"
+                            id="ville"
+                            value={ville}
+                            onChange={formik.handleChange}
+                            required
+                        />
+                        <label htmlFor="zipcode">Code postale : </label>
+                        <input
+                            type="number"
+                            name="zipcode"
+                            id="zipcode"
+                            value={zipcode}
+                            onChange={formik.handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="email">Adresse email électronique : </label>
                         <input
                             type="email"
                             name="email"
                             id="email"
                             value={email}
                             onChange={formik.handleChange}
+                            required
                         />
+                    </div>
+
+                    <div>
+                        <label htmlFor="password">Mot de passe : </label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            value={password}
+                            onChange={formik.handleChange}
+                        />
+
+                        <label htmlFor="passwordConfirmation">
+                            Confirmation de mot de passe :{' '}
+                        </label>
+                        <input
+                            type="password"
+                            name="passwordConfirmation"
+                            id="passwordConfirmation"
+                            value={passwordConfirmation}
+                            onChange={formik.handleChange}
+                        />
+                    </div>
+                    <div>
+                        <input type="checkbox" name="cgu" id="cgu" />
+                        <label className="cgu" htmlFor="cgu">
+                            J'accepte{' '}
+                            <a href="#" _target="blank">
+                                les conditions d'utilisations.
+                            </a>{' '}
+                        </label>
+                    </div>
+
+                    <div className="submit">
+                        <button type="submit">Créer mon compte</button>
                     </div>
                 </form>
             </div>
