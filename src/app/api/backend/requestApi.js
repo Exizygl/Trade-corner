4a0fd670967c515a38517a98e2b4034cfcd85462
JordanMahaut <jordanmahaut09@gmail.com>
2022-06-24 10:51:05 +0200

    Changement de route requestAPI

--- src/app/api/backend/requestApi.js
+++ src/app/api/backend/requestApi.js
@@ -1,10 +1,9 @@
-
 import apiBackEnd from './api.Backend';
 
 export const register = (values) => {
-    apiBackEnd.post("auth/register", values)
-}
+    apiBackEnd.post('user/register', values);
+};
 
 export const login = (values) => {
-    apiBackEnd.post("auth/login", values)
-}
+    apiBackEnd.post('user/login', values);
+};
