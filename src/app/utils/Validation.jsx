32e9fa3e7d29fdccf49663242c69e44560533eb1
JordanMahaut <jordanmahaut09@gmail.com>
2022-07-25 17:03:53 +0200

    ValidationSchema (pseudo,email,password,passwordConfirmation,phoneNumber)

---
+++ src/app/utils/Validation.jsx
@@ -0,0 +1,26 @@
+import * as Yup from 'yup';
+
+// Regex
+
+const regExPassword = /^(?=.$\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+const regExNumberPhone =
+    /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,3})[ \\-]*)*?[0-9]{2,3}?[ \\-]*[0-9]{2,3}?$/;
+
+export default Yup.object().shape({
+    pseudo: Yup.string()
+        .required('Le pseudo est requis !')
+        .min(3, 'Le pseudonyme doit avoir au moins 3 caractères !'),
+    email: Yup.string()
+        .email('Veuillez saisir une adresse mail valide !')
+        .required('Une adresse mail est requis !'),
+    password: Yup.string()
+        .required('Le champ mot de passe est requis !')
+        .min(8, 'Le mot de passe doit avoir au moins 8 caractères !')
+        .matches(regExPassword, 'Veuillez créer un mot de passe plus fort !'),
+    passwordConfirmation: Yup.string()
+        .required('Le champ de confirmation du mot de passe est requis !')
+        .oneOf([Yup.ref('password'), null], 'Les mots de passes ne correspondent pas !'),
+    phoneNumber: Yup.string()
+        .required('Un numéro de téléphone est requis !')
+        .matches(regExNumberPhone, 'Le numéro de téléphone est invalide !'),
+});
