ab6769d658f7da38e6c82f419c886cf72c2787f1
JordanMahaut <jordanmahaut09@gmail.com>
2022-07-27 10:22:25 +0200

    Modification register

--- src/app/components/layouts/Register.jsx
+++ src/app/components/layouts/Register.jsx
@@ -63,154 +63,191 @@ const Register = () => {
         <div>
             <div className="global">
                 <form onSubmit={handleSubmit}>
-                    <div>
-                        <h1>Inscription</h1>
-                    </div>
+                    <h1>Inscription</h1>
 
-                    <div>
-                        <label htmlFor="pseudo">Pseudonyme : </label>
-                        <input
-                            type="text"
-                            name="pseudo"
-                            id="pseudo"
-                            value={values.pseudo}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                        />
-                        {touched.pseudo && errors.pseudo ? (
-                            <small>{errors.pseudo}</small>
-                        ) : (
-                            ''
-                        )}
-                    </div>
+                    <div className="form">
+                        <div className="Information">
+                            <label htmlFor="pseudo">Pseudonyme : </label>
 
-                    <div className="Information">
-                        <label htmlFor="name">Nom complet : </label>
-                        <input
-                            type="text"
-                            name="name"
-                            id="surName"
-                            value={values.name}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                        />
-                        {touched.name && errors.name ? <small>{errors.name}</small> : ''}
+                            <input
+                                type="text"
+                                name="pseudo"
+                                id="pseudo"
+                                value={values.pseudo}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                            <div>
+                                {touched.pseudo && errors.pseudo ? (
+                                    <small>{errors.pseudo}</small>
+                                ) : (
+                                    ''
+                                )}
+                            </div>
 
-                        <label htmlFor="phoneNumber">Numéro de téléphone : </label>
-                        <input
-                            type="tel"
-                            name="phoneNumber"
-                            id="phoneNumber"
-                            value={values.phoneNumber}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                        />
-                        {touched.phoneNumber && errors.phoneNumber ? (
-                            <small>{errors.phoneNumber}</small>
-                        ) : (
-                            ''
-                        )}
-                    </div>
+                            <label htmlFor="name">Nom complet : </label>
 
-                    <div>
-                        <label htmlFor="adress">Adresse postale : </label>
-                        <input
-                            type="text"
-                            name="adress"
-                            id="adress"
-                            value={values.adress}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                        />
-                        {touched.adress && errors.adress ? (
-                            <small>{errors.adress}</small>
-                        ) : (
-                            ''
-                        )}
-                    </div>
+                            <input
+                                type="text"
+                                name="name"
+                                id="surName"
+                                value={values.name}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                            <div>
+                                {touched.name && errors.name ? (
+                                    <small>{errors.name}</small>
+                                ) : (
+                                    ''
+                                )}
+                            </div>
+                        </div>
 
-                    <div>
-                        <label htmlFor="ville">Ville : </label>
-                        <input
-                            type="text"
-                            name="ville"
-                            id="ville"
-                            value={values.ville}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                        />
-                        {touched.ville && errors.ville ? (
-                            <small>{errors.ville}</small>
-                        ) : (
-                            ''
-                        )}
+                        <div>
+                            <label htmlFor="phoneNumber">Numéro de téléphone : </label>
 
-                        <label htmlFor="zipcode">Code postale : </label>
-                        <input
-                            type="text"
-                            name="zipcode"
-                            id="zipcode"
-                            value={values.zipcode}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                        />
-                        {touched.zipcode && errors.zipcode ? (
-                            <small>{errors.zipcode}</small>
-                        ) : (
-                            ''
-                        )}
-                    </div>
+                            <input
+                                type="tel"
+                                name="phoneNumber"
+                                id="phoneNumber"
+                                value={values.phoneNumber}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
 
-                    <div>
-                        <label htmlFor="email">Adresse email électronique : </label>
-                        <input
-                            type="email"
-                            name="email"
-                            id="email"
-                            value={values.email}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                        />
-                        {touched.email && errors.email ? (
-                            <small>{errors.email}</small>
-                        ) : (
-                            ''
-                        )}
-                    </div>
+                            <div>
+                                {touched.phoneNumber && errors.phoneNumber ? (
+                                    <small>{errors.phoneNumber}</small>
+                                ) : (
+                                    ''
+                                )}
+                            </div>
+                        </div>
 
-                    <div>
-                        <label htmlFor="password">Mot de passe : </label>
-                        <input
-                            type="password"
-                            name="password"
-                            id="password"
-                            placeholder="Ex : Test123!"
-                            value={values.password}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                        />
-                        {touched.password && errors.password ? (
-                            <small>{errors.password}</small>
-                        ) : (
-                            ''
-                        )}
+                        <div>
+                            <label htmlFor="adress">Adresse postale : </label>
 
-                        <label htmlFor="passwordConfirmation">
-                            Confirmation de mot de passe :{' '}
-                        </label>
-                        <input
-                            type="password"
-                            name="passwordConfirmation"
-                            id="passwordConfirmation"
-                            value={values.passwordConfirmation}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                        />
-                        {touched.passwordConfirmation && errors.passwordConfirmation ? (
-                            <small>{errors.passwordConfirmation}</small>
-                        ) : (
-                            ''
-                        )}
+                            <input
+                                type="text"
+                                name="adress"
+                                id="adress"
+                                value={values.adress}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                            <div>
+                                {touched.adress && errors.adress ? (
+                                    <small>{errors.adress}</small>
+                                ) : (
+                                    ''
+                                )}
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="ville">Ville : </label>
+
+                            <input
+                                type="text"
+                                name="ville"
+                                id="ville"
+                                value={values.ville}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                            <div>
+                                {touched.ville && errors.ville ? (
+                                    <small>{errors.ville}</small>
+                                ) : (
+                                    ''
+                                )}
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="zipcode">Code postale : </label>
+
+                            <input
+                                type="text"
+                                name="zipcode"
+                                id="zipcode"
+                                value={values.zipcode}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                            <div>
+                                {touched.zipcode && errors.zipcode ? (
+                                    <small>{errors.zipcode}</small>
+                                ) : (
+                                    ''
+                                )}
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="email">Adresse email électronique : </label>
+
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                value={values.email}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                            <div>
+                                {touched.email && errors.email ? (
+                                    <small>{errors.email}</small>
+                                ) : (
+                                    ''
+                                )}
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="password">Mot de passe : </label>
+
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                placeholder="Ex : Test123!"
+                                value={values.password}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                            <div>
+                                {touched.password && errors.password ? (
+                                    <small>{errors.password}</small>
+                                ) : (
+                                    ''
+                                )}
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="passwordConfirmation">
+                                Confirmation mot de passe :{' '}
+                            </label>
+
+                            <input
+                                type="password"
+                                name="passwordConfirmation"
+                                id="passwordConfirmation"
+                                value={values.passwordConfirmation}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                            <div>
+                                {touched.passwordConfirmation &&
+                                errors.passwordConfirmation ? (
+                                    <small>{errors.passwordConfirmation}</small>
+                                ) : (
+                                    ''
+                                )}
+                            </div>
+                        </div>
                     </div>
 
                     <div className="submit">
